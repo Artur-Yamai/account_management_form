4bc323b229b92ddbbedce8250a54ef698ebd9223
Iamaletdinov Artur <willbefine1@gmail.com>
2025-03-07 13:36:11 +0500

    Header вынесен в отдельынй компонент

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,16 @@
 <template>
-  <div class="header-with-button">
-    <h1>Главный заголовок страницы</h1>
-    <button @click="addAccount" class="btn btn-primary">+</button>
-  </div>
+  <Header />
   <AccountForm />
 </template>
 
 <script setup lang="ts">
+import Header from "./components/Header.vue";
 import AccountForm from "./components/AccountForm.vue";
-import { useStore } from "./store";
-
-const store = useStore();
-
-const addAccount = () => store.addAccount();
-addAccount();
 </script>
 
 <style>
 #app {
   max-width: 1080px;
   margin: 0 auto;
-}
-
-.header-with-button {
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  margin: 0.75rem;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    <h1>Главный заголовок страницы</h1>
+    <button @click="addAccount" class="btn btn-primary">+</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useStore } from "@/store";
+
+const store = useStore();
+
+const addAccount = () => store.addAccount();
+addAccount();
+</script>
+
+<style>
+.header {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin: 0.75rem;
+}
+</style>
